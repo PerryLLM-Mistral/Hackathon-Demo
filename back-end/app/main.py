7fa1ea56f9eb0244468e2a7f02150273cc7e28fd
D3n3bX <laia.delgado@live.u-tad.com>
2026-02-28 16:46:19 +0100

    Refactor: move websocket to routes

--- back-end/app/main.py
+++ back-end/app/main.py
@@ -3,7 +3,7 @@ from app.multi_llm.orchestrator import Orchestrator
 from app.simulation.engine import SimulationEngine
 from app.database import engine as db_engine
 from app.src.models import models
-from app.src.routes import countries, relationships
+from app.src.routes import countries, relationships, simulation, ws
 
 # Create the database tables
 models.Base.metadata.create_all(bind=db_engine)
@@ -16,3 +16,5 @@ sim_engine = SimulationEngine()
 # Include models routes
 app.include_router(countries.router)
 app.include_router(relationships.router)
+app.include_router(simulation.router)
+app.include_router(ws.router)
