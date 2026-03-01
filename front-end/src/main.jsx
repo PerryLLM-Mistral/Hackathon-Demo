32c2b3beb8955b6f249862d092dae305825772c9
aperezmarquez <aperezmarquez.business@gmail.com>
2026-03-01 17:05:08 +0100

    Added: Websocket connection, removed Strictmode for dev

--- front-end/src/main.jsx
+++ front-end/src/main.jsx
@@ -3,8 +3,9 @@ import { createRoot } from 'react-dom/client'
 import './index.sass'
 import App from './App.jsx'
 
+// TODO: StrictMode is removed so the websocket works correctly in dev, need to added it back for production
 createRoot(document.getElementById('root')).render(
-    <StrictMode>
+//    <StrictMode>
         <App />
-    </StrictMode>,
+//    </StrictMode>,
 )
