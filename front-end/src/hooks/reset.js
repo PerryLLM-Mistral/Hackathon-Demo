7184841f3636272d7eb317832d7817186076f844
aperezmarquez <aperezmarquez.business@gmail.com>
2026-03-01 14:23:56 +0100

    Added: reset countries hook

---
+++ front-end/src/hooks/reset.js
@@ -0,0 +1,20 @@
+const BASE_URL = `${import.meta.env.VITE_PUBLIC_API_URL}/reset`;
+
+const resetCountries = async () => {
+    try {
+        const response = await fetch(`${BASE_URL}/reset`, {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+            }
+        });
+
+        if (!response.ok) {
+            throw new Error("Error while reseting the countries");
+        }
+        const data = response.json();
+        return data;
+    } catch (err) {
+        throw new Error("Error reseting")
+    }
+}
